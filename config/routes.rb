352408d73956d267a1629b18d35ddfac13bb4dc2
Sachin Srivastava <sachinrising@gmail.com>
2015-10-24 19:52:44 +0530

    Forms Added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   resources :contacts
   get '/about' => 'pages#about'
-  get '/count' => 'pages#count'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
