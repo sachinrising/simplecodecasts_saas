d5f09091dd50afc9955e8d4f868688dc83ea5f89
Sachin Srivastava <sachinrising@gmail.com>
2015-10-30 07:56:45 +0530

    Some improvement

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+
+	before_filter :select_plan, only: :new
+	
 	def create
 		super do |resource|
 			if params[:plan]
@@ -6,9 +9,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
 				if resource.plan_id == 2
 					resource.save_with_payment
 				else
-					resource.save_with_payment
+					resource.save
 				end
 			end
 		end
 	end
+
+	private
+	def select_plan
+		unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+			flash[:notice] = "Please select a membership plan to sign up"
+			redirect_to root_url 
+		end
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable
-
+         :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
   attr_accessor :stripe_card_token
-
+  
   def save_with_payment
-  	if valid?
-  		customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_tokem)
-  		self.stripe_customer_toke = customer.id
-  		save!
-  	end
+    if valid?
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      self.stripe_customer_token = customer.id
+      save!
+    end
   end
 end
